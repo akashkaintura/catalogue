0a223ddfa785507a74f9ae5aca66327a26cc3bd6
Akash Kaintura <akashkaintura@Akashs-MacBook-Pro.local>
2021-08-19 10:54:04 +0530

    Await-async, Animations and UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,15 @@
 import 'package:catalogue/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -13,7 +21,7 @@ class LoginPage extends StatelessWidget {
           ),
           SizedBox(height: 20),
           Text(
-            "Welcome",
+            "Welcome $name",
             style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 20),
@@ -24,20 +32,47 @@ class LoginPage extends StatelessWidget {
                 TextFormField(
                   decoration: InputDecoration(
                       hintText: "Enter username", labelText: "Username"),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
                   decoration: InputDecoration(
                       hintText: "Enter password", labelText: "password"),
                 ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
+                SizedBox(height: 40),
+                InkWell(
+                  onTap: () async {
+                    setState(() {
+                      changeButton = true;
+                    });
+                    await Future.delayed(Duration(seconds: 1));
                     Navigator.pushNamed(context, MyRoutes.homeRoute);
                   },
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(
-                    minimumSize: Size(150, 40),
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    width: changeButton ? 50 : 150,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: changeButton
+                        ? Icon(Icons.done, color: Colors.white)
+                        : Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      // shape:
+                      //     ChangeButton ? BoxShape.circle : BoxShape.rectangle,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 20 : 8),
+                    ),
                   ),
                 ),
               ],
